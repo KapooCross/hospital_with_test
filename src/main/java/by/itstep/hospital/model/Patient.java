bec0e7167f14aa048190985b8e90ae8d5ae063a1
jb <kapoocross@gmail.com>
2024-01-15 23:16:40 +0300

    Initial commit

---
+++ src/main/java/by/itstep/hospital/model/Patient.java
@@ -0,0 +1,43 @@
+package by.itstep.hospital.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldNameConstants;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@FieldNameConstants
+@Entity
+@Table(name = "patients")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="full_name", length = 50)
+    private String fullName;
+
+    @Column(name="gender", length = 8)
+    private String gender;
+
+    @Column(name="date_of_birth", columnDefinition = "DATE")
+    private LocalDate dateOfBirth;
+
+    @Column(name="diagnosis", length = 100)
+    private String diagnosis;
+
+    @Column(name="date_of_admission", columnDefinition = "DATE")
+    private LocalDate dateOfAdmission;
+
+    @Column(name="date_of_discharge", columnDefinition = "DATE")
+    private LocalDate dateOfDischarge;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
---
+++ src/main/java/by/itstep/hospital/service/PatientServiceImpl.java
@@ -0,0 +1,69 @@
+package by.itstep.hospital.service;
+
+import by.itstep.hospital.model.Patient;
+import by.itstep.hospital.repository.PatientRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class PatientServiceImpl implements PatientService {
+
+    private final PatientRepository patientRepository;
+
+    @Override
+    public Patient save(Patient patient) {
+        return this.patientRepository.save(patient);
+    }
+
+    @Override
+    public Optional<Patient> findById(Long id) {
+        return this.patientRepository.findById(id);
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return this.patientRepository.existsById(id);
+    }
+
+    @Override
+    public Iterable<Patient> findAll() {
+        return this.patientRepository.findAll();
+    }
+
+    @Override
+    public Page<Patient> findPaginated(int pageNumber, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize);
+        return this.patientRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Patient> findPaginatedAndSorted(int pageNumber, int pageSize, String sortField, String sortDirection) {
+        /*Sort sort;
+        if (sortDirection.equalsIgnoreCase("asc"))
+            sort = Sort.by(sortField).ascending();
+        else
+            sort = Sort.by(sortField).descending();*/
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending():
+                Sort.by(sortField).descending();
+        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize, sort);
+        return this.patientRepository.findAll(pageable);
+    }
+
+    @Override
+    public void deleteById(Long id){
+        this.patientRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(Patient patient) {
+        this.patientRepository.delete(patient);
+    }
+}
